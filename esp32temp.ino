125471b3fe583a775af5ccebb88ae9f8113b1690
Aditya Lal <111847116+aditya17lal@users.noreply.github.com>
2024-11-15 23:12:46 +0530

    Update esp32temp.ino

--- esp32temp.ino
+++ esp32temp.ino
@@ -1,4 +1,3 @@
-//5.1
 const int tempPin = 33;            
 int Value; 
 double milivlt,Cel,Far; 
@@ -51,3 +50,35 @@ Serial.println("*F");
 delay(2000);                       
 //check the temperature every 2 second
 }
+
+// 1. Connect P1 port and SV12 port using FRC cable as shown above
+// 2. Connect the USB cable to the board
+// 3. Open Arduino IDE .Select DOIT ESP32 DEVKIT V1in boards and select COM port.
+// 4. Write the program, verify and Upload it.
+// 5. Now you can see the output on the serial monitor
+
+const int tempPin = 39; // pin 33 of port P1 connected to LM35 output
+
+int Value;
+double milivlt,Cel,Far;
+
+void setup()
+{
+Serial.begin(9600);
+}
+
+void loop()
+{
+ Value = analogRead(tempPin); //read sensor output value
+ milivlt = (Value / 2048.0) * 3300; // converting it to millivots.
+ Cel = milivlt * 0.1; // calculating temperature in Celsius
+ Far = (Cel * 1.8) + 32; // convert from C to Fahrenheit
+ Serial.print(" Temperature in Celsius = ");
+ Serial.print(Cel);
+ Serial.print("*C");
+ Serial.print("\t Temperature in Fahrenheit = ");
+ Serial.print(Far);
+ Serial.println("*F");
+ delay(2000); //check the temperature every 2 second
+}
+
